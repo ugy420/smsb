7678de2503e7d65dca63e1f5d9f62c8ffab78f02
Dorji Phuntsho <dorjiphuntsho1491@gmail.com>
2024-10-14 22:40:51 +0530

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Make sure this file includes the necessary styles
+import './Navbar.css'; 
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-blue-500 to-blue-700 shadow-lg p-10 fixed top-0 left-0 right-0 z-50">
+    <nav className="bg-gradient-to-r from-blue-500 to-blue-700 shadow-lg p-6 fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold">
-          <img src=''/>
-          Sports Ground Booking
+        {/* Logo and Title */}
+        <div className="flex items-center space-x-4">
+          <span className="text-white text-2xl font-bold">Sports Ground Booking</span>
         </div>
+
+        {/* Navigation Links */}
         <div className="space-x-10">
           <Link to="/" className="text-white hover:text-blue-400 transition duration-300">
             Home
