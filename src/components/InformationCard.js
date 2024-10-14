86e125824b3509336d2c79f5067b0790e4ca4b1c
Ugyen Wangda Gyeltshen <ugyenwgyeltshen.ug@gmail.com>
2024-10-14 19:58:29 +0600

    ttt

--- src/components/InformationCard.js
+++ src/components/InformationCard.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const InfoCard = ({ icon, title, number }) => {
   return (
-    <div className="bg-gray-100 rounded-lg p-6 shadow-md flex items-center space-x-4">
-      <div className="icon bg-gray-300 rounded-full p-3">
-        <img src={icon} alt={`${title} icon`} className="h-8 w-8" />
+    <div className="bg-white rounded-lg p-6 shadow-md flex items-center space-x-4 w-full md:w-72">
+      <div className="bg-gray-300 rounded-full p-3">
+        <img src={icon} alt={`${title} icon`} className="h-12 w-12" />
       </div>
-      <div className="text">
+      <div>
         <h3 className="text-xl font-semibold">{title}</h3>
-        <p className="text-x0.5 font-bold">{number}</p>
+        <p className="text-lg font-bold">{number}</p>
       </div>
     </div>
   );
 };
+
 
 export default InfoCard;
