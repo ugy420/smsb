2f98b8620b6ae5993aa97108d046f02a7d58a5d3
Ugyen Wangda Gyeltshen <ugyenwgyeltshen.ug@gmail.com>
2024-11-12 00:55:22 +0600

    timeslots

--- src/pages/BookGround.js
+++ src/pages/BookGround.js
@@ -33,8 +33,8 @@ const BookingForm = ({ groundType, date, groundId, onSuccess }) => {
     const day = date.getDay();
 
     const times = day === 0 || day === 6
-      ? ['9:00 AM - 10:00 AM', '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 1:00 PM','1:00 PM - 2:00 PM','2:00 PM - 3:00 PM','3:00 PM - 4:00 PM','4:00 PM - 5:00 PM', '5:00 PM - 6:00 PM']
-      : ['4:00 PM - 5:00 PM', '5:00 PM - 6:00 PM'];
+      ? ['9:00 AM - 10:00 AM', '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 1:00 PM','1:00 PM - 2:00 PM','2:00 PM - 3:00 PM','3:00 PM - 4:00 PM','4:00 PM - 5:00 PM', '5:00 PM - 6:00 PM', '6:00 PM - 7:00 PM', '7:00 PM - 8:00 PM', '8:00 PM - 9:00 PM', '9:00 PM - 10:00 PM']
+      : ['4:00 PM - 5:00 PM', '5:00 PM - 6:00 PM', '6:00 PM - 7:00 PM', '7:00 PM - 8:00 PM', '8:00 PM - 9:00 PM', '9:00 PM - 10:00 PM'];
 
     setTimeOptions(times);
   };
